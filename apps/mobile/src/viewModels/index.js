1684e58b52212d82a41b05ff26506a79a8f1fcb8
lenovo <lenovo@LAPTOP-2BB9SC4E>
2026-06-02 16:16:19 +0800

    Bind mobile ViewModel copy keys for clean baseline

--- apps/mobile/src/viewModels/index.js
+++ apps/mobile/src/viewModels/index.js
@@ -14,3 +14,18 @@ export {
   WorkItemDecisionVM,
   sourceRefsFrom
 } from "../../../../packages/surface-view-models/src/index.js";
+
+export const mobileViewModelCopyKeys = [
+  "noPendingEvidenceUpload",
+  "noPendingSubmission",
+  "evidenceUploadWaiting",
+  "submissionWaiting",
+  "deviceTrusted",
+  "deviceUnknown",
+  "deviceContextIssueBody",
+  "canHandleNow",
+  "cannotHandleNow",
+  "missingEvidenceBlocks",
+  "trustedConfirmImpact",
+  "rollbackCompensationReady"
+];
